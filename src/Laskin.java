1822aa949c132f673220030ca53b52d6a2270322
71m05h1k <rasmer@mail.ru>
2015-06-23 00:09:14 +0300

    calculate me!

--- src/Laskin.java
+++ src/Laskin.java
@@ -7,56 +7,129 @@ import java.awt.*;
 public class Laskin {
     Laskin (){
         JFrame jfrm = new JFrame("Calculator");
-        jfrm.setSize(500 , 500);
+        jfrm.setSize(200 , 200);
         jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jfrm.setLayout(new GridBagLayout());
-//C  =
-//789/
-//456*
-//123+
-//0  -
-        JLabel button_empty = new JLabel("[ ]");
-        JLabel button_c = new JLabel("[C]");
-        JLabel button_0 = new JLabel("[0]");
-        JLabel button_1 = new JLabel("[1]");
-        JLabel button_2 = new JLabel("[2]");
-        JLabel button_3 = new JLabel("[3]");
-        JLabel button_4 = new JLabel("[4]");
-        JLabel button_5 = new JLabel("[5]");
-        JLabel button_6 = new JLabel("[6]");
-        JLabel button_7 = new JLabel("[7]");
-        JLabel button_8 = new JLabel("[8]");
-        JLabel button_9 = new JLabel("[9]");
-        JLabel button_plus = new JLabel("[+]");
-        JLabel button_minus = new JLabel("[-]");
-        JLabel button_mul = new JLabel("[X]");
-        JLabel button_div = new JLabel("[_]");
-        JLabel button_summ = new JLabel("[=]");
+        GridBagConstraints gBC = new GridBagConstraints();
 
-        jfrm.add(button_c);
-//        jfrm.add(button_empty);
-//        jfrm.add(button_empty);
-//        jfrm.add(button_summ);
-//
-//        jfrm.add(button_7);
-//        jfrm.add(button_8);
-//        jfrm.add(button_9);
-//        jfrm.add(button_div);
-//
-//        jfrm.add(button_4);
-//        jfrm.add(button_5);
-//        jfrm.add(button_6);
-//        jfrm.add(button_mul);
-//
-//        jfrm.add(button_1);
-//        jfrm.add(button_2);
-//        jfrm.add(button_3);
-//        jfrm.add(button_plus);
-//
-//        jfrm.add(button_0);
-//        jfrm.add(button_empty);
-//        jfrm.add(button_empty);
-//        jfrm.add(button_minus);
+        JLabel button_c = new JLabel("C");
+        gBC.gridheight = 1;
+        gBC.gridwidth = 1;
+        gBC.gridx = 0;
+        gBC.gridy = 1;
+        jfrm.add(button_c,gBC);
+
+        JLabel button_0 = new JLabel("0");
+        gBC.gridheight = 1;
+        gBC.gridwidth = 1;
+        gBC.gridx = 1;
+        gBC.gridy = 4;
+        jfrm.add(button_0,gBC);
+
+        JLabel button_1 = new JLabel("1");
+        gBC.gridheight = 1;
+        gBC.gridwidth = 1;
+        gBC.gridx = 1;
+        gBC.gridy = 3;
+        jfrm.add(button_1,gBC);
+
+        JLabel button_2 = new JLabel("2");
+        gBC.gridheight = 1;
+        gBC.gridwidth = 1;
+        gBC.gridx = 2;
+        gBC.gridy = 3;
+        jfrm.add(button_2,gBC);
+
+        JLabel button_3 = new JLabel("3");
+        gBC.gridheight = 1;
+        gBC.gridwidth = 1;
+        gBC.gridx = 3;
+        gBC.gridy = 3;
+        jfrm.add(button_3,gBC);
+
+        JLabel button_4 = new JLabel("4");
+        gBC.gridheight = 1;
+        gBC.gridwidth = 1;
+        gBC.gridx = 1;
+        gBC.gridy = 2;
+        jfrm.add(button_4,gBC);
+
+        JLabel button_5 = new JLabel("5");
+        gBC.gridheight = 1;
+        gBC.gridwidth = 1;
+        gBC.gridx = 2;
+        gBC.gridy = 2;
+        jfrm.add(button_5,gBC);
+
+        JLabel button_6 = new JLabel("6");
+        gBC.gridheight = 1;
+        gBC.gridwidth = 1;
+        gBC.gridx = 3;
+        gBC.gridy = 2;
+        jfrm.add(button_6,gBC);
+
+        JLabel button_7 = new JLabel("7");
+        gBC.gridheight = 1;
+        gBC.gridwidth = 1;
+        gBC.gridx = 1;
+        gBC.gridy = 1;
+        jfrm.add(button_7,gBC);
+
+        JLabel button_8 = new JLabel("8");
+        gBC.gridheight = 1;
+        gBC.gridwidth = 1;
+        gBC.gridx = 2;
+        gBC.gridy = 1;
+        jfrm.add(button_8,gBC);
+
+        JLabel button_9 = new JLabel("9");
+        gBC.gridheight = 1;
+        gBC.gridwidth = 1;
+        gBC.gridx = 3;
+        gBC.gridy = 1;
+        jfrm.add(button_9,gBC);
+
+        JLabel button_plus = new JLabel("+");
+        gBC.gridheight = 2;
+        gBC.gridwidth = 1;
+        gBC.gridx = 4;
+        gBC.gridy = 3;
+        jfrm.add(button_plus,gBC);
+
+        JLabel button_minus = new JLabel("-");
+        gBC.gridheight = 1;
+        gBC.gridwidth = 1;
+        gBC.gridx = 4;
+        gBC.gridy = 2;
+        jfrm.add(button_minus,gBC);
+
+        JLabel button_mul = new JLabel("X");
+        gBC.gridheight = 1;
+        gBC.gridwidth = 1;
+        gBC.gridx = 4;
+        gBC.gridy = 1;
+        jfrm.add(button_mul,gBC);
+
+        JLabel button_div = new JLabel("/");
+        gBC.gridheight = 1;
+        gBC.gridwidth = 1;
+        gBC.gridx = 5;
+        gBC.gridy = 1;
+        jfrm.add(button_div,gBC);
+
+        JLabel button_summ = new JLabel("=");
+        gBC.gridheight = 1;
+        gBC.gridwidth = 1;
+        gBC.gridx = 3;
+        gBC.gridy = 4;
+        jfrm.add(button_summ,gBC);
+
+        JTextField jtf = new JTextField("7350.1505");
+        gBC.gridheight = 1;
+        gBC.gridwidth = 6;
+        gBC.gridx = 0;
+        gBC.gridy = 0;
+        jfrm.add(jtf,gBC);
 
         jfrm.setVisible(true);
     }
