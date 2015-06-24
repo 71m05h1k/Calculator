997f560e1e65674d49510107b401c907296da05d
71m05h1k <rasmer@mail.ru>
2015-06-24 17:05:42 +0300

    Calculate me!

--- src/other.java
+++ src/other.java
@@ -1,7 +1,35 @@
 public class other {
     public static String rezultat(String s){
-        String z = s;
-        z = "rezultat!";
-        return z;
+    if (s.length() == 0) {
+            return "error!";
+        }
+    String[] tokens = s.split(" ");
+    if (tokens.length != 3) {
+        return "incorrect lenght!";
+    }
+        return (vichislyaem(tokens[0] , tokens[1], tokens[2]));
+    }
+
+    private static String vichislyaem(String token_0, String token_1, String token_2) {
+    int numb_1 = Integer.parseInt(token_0);
+    int numb_2 = Integer.parseInt(token_1);
+    int rez;
+        if (token_2.equals("+")) {
+        rez = numb_1 + numb_2;
+        }
+        else if (token_2.equals("-")) {
+            rez = numb_1 - numb_2;
+        }
+        else if (token_2.equals("Х")) {
+            rez = numb_1 * numb_2;
+        }
+
+        else if (token_2.equals("/")) {
+            rez = numb_1 / numb_2;
+        }
+        else {
+            return "hueta kakayato!";
+        }
+    return Integer.toString(rez);
     }
 }
