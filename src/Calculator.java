f5717b4491015ff786032d4d8f5e7c9383228f82
Andrew <rasmer@tut.by>
2016-10-30 13:52:17 +0300

    Some correction

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,11 +4,6 @@ import javax.swing.*;
 
 public class Calculator {
     public static void main(String[] arguments) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new Laskin();
-            }
-        });
+        new Laskin();
     }
 }
